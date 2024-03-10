440f364c8faa5a0fa553736753ffd1c343a0ed25
Oleg Bask <obask@users.noreply.github.com>
2024-03-09 23:30:54 -0800

    Initial Commit

---
+++ TryLexical/TryLexicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryLexicalApp.swift
+//  TryLexical
+//
+//  Created by Oleg Bask on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TryLexicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
